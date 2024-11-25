ab4de68991764f557edef066e4d3877ecc827786
WilliamEmami <62527283+WilliamEmami@users.noreply.github.com>
2024-11-25 17:00:24 +0100

    refactorization begun

--- Assets/Scripts/Refactored scripts/CartLinkBuilder.cs
+++ Assets/Scripts/Refactored scripts/CartLinkBuilder.cs
@@ -6,39 +6,21 @@ using UnityEngine;
 
 public class CartLinkBuilder : MonoBehaviour
 {
-    public LENGTH length = LENGTH.THREE;
-    public Transform lastRodTransform;
-    //public AbstractTrainCart first, second, third, fourth, fifth;
     [SerializeField] public List<AbstractTrainCart> list;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        lastRodTransform = gameObject.transform;
-        switch (length)
+        if (list.Count != 0)
         {
-            case LENGTH.THREE:
+            //AbstractTrainCart current = stack.Pop();
+            //current.transform.position = gameObject.transform.position;
+            while (list.Count > 0)
+            {
                 
-                break;
-            case LENGTH.FIVE:
-                // code block
-                break;
-            default:
-                // code block
-                break;
+            }
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-}
-
-public enum LENGTH : int
-{
-    THREE = 3,
-    FIVE = 5
 }
